df7e776e177f54eabe4ff0f84aa31e406114b2ae
Marcin Bilski <gyamtso@gmail.com>
2017-11-30 12:21:02 +0100

    Extract metric reporting.

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -1,14 +1,11 @@
 package pipeline
 
 import (
-	"fmt"
 	"github.com/fatih/color"
 	"github.com/nowthisnews/dp-pubsub-archai/metrics"
-	"github.com/olekukonko/tablewriter"
 	"golang.org/x/net/context"
 	"log"
 	"math/rand"
-	"os"
 	"sync"
 	"time"
 )
@@ -30,18 +27,7 @@ func Run(ctx context.Context, tick time.Duration, wg *sync.WaitGroup) {
 	batchCh, batchPermitCh := dispatch(ctx, tick, taskCh, taskPermitCh, &dispatcherMetrics, wg)
 	consumerMetrics := metrics.New()
 	consume(ctx, batchCh, batchPermitCh, &consumerMetrics, wg)
-
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case <-time.After(time.Second * 5):
-				ReportMetrics(producerMetrics, dispatcherMetrics, consumerMetrics)
-			}
-		}
-	}()
+	ReportPeriodically(ctx, time.Second*5, &producerMetrics, &dispatcherMetrics, &consumerMetrics, wg)
 }
 
 type Task int64
@@ -70,25 +56,6 @@ func (batch Batch) AddTask(task Task) (Batch, error) {
 	// limit the maximum batch size here.
 	newBatch := append(batch, task)
 	return newBatch, nil
-}
-
-func ReportMetrics(producerMetrics, dispatcherMetrics, consumerMetrics metrics.Metrics) {
-	report([]string{"source", "iterations", "successes", "failures"},
-		[][]string{
-			[]string{"producer", fmt.Sprintf("%v", producerMetrics.Iterations), fmt.Sprintf("%v", producerMetrics.Successes), fmt.Sprintf("%v", producerMetrics.Failures)},
-			[]string{"dispatch", fmt.Sprintf("%v", dispatcherMetrics.Iterations), fmt.Sprintf("%v", dispatcherMetrics.Successes), fmt.Sprintf("%v", dispatcherMetrics.Failures)},
-			[]string{"consumer", fmt.Sprintf("%v", consumerMetrics.Iterations), fmt.Sprintf("%v", consumerMetrics.Successes), fmt.Sprintf("%v", consumerMetrics.Failures)},
-		},
-	)
-}
-
-func report(header []string, data [][]string) {
-	table := tablewriter.NewWriter(os.Stderr) // TODO: Write to log.
-	table.SetHeader(header)
-	for _, row := range data {
-		table.Append(row)
-	}
-	table.Render()
 }
 
 // ASK:
---
+++ pipeline/report.go
@@ -0,0 +1,44 @@
+package pipeline
+
+import (
+	"fmt"
+	"github.com/nowthisnews/dp-pubsub-archai/metrics"
+	"github.com/olekukonko/tablewriter"
+	"golang.org/x/net/context"
+	"os"
+	"sync"
+	"time"
+)
+
+func ReportPeriodically(ctx context.Context, tick time.Duration, producerMetrics, dispatcherMetrics, consumerMetrics *metrics.Metrics, wg *sync.WaitGroup) {
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(time.Second * 5):
+				ReportMetrics(*producerMetrics, *dispatcherMetrics, *consumerMetrics)
+			}
+		}
+	}()
+}
+
+func ReportMetrics(producerMetrics, dispatcherMetrics, consumerMetrics metrics.Metrics) {
+	printTable([]string{"source", "iterations", "successes", "failures"},
+		[][]string{
+			[]string{"producer", fmt.Sprintf("%v", producerMetrics.Iterations), fmt.Sprintf("%v", producerMetrics.Successes), fmt.Sprintf("%v", producerMetrics.Failures)},
+			[]string{"dispatch", fmt.Sprintf("%v", dispatcherMetrics.Iterations), fmt.Sprintf("%v", dispatcherMetrics.Successes), fmt.Sprintf("%v", dispatcherMetrics.Failures)},
+			[]string{"consumer", fmt.Sprintf("%v", consumerMetrics.Iterations), fmt.Sprintf("%v", consumerMetrics.Successes), fmt.Sprintf("%v", consumerMetrics.Failures)},
+		},
+	)
+}
+
+func printTable(header []string, data [][]string) {
+	table := tablewriter.NewWriter(os.Stderr) // TODO: Write to log.
+	table.SetHeader(header)
+	for _, row := range data {
+		table.Append(row)
+	}
+	table.Render()
+}

71fabb09bd542cd80ef69787ce0e35af652b5ba5
Marcin Bilski <gyamtso@gmail.com>
2017-11-30 12:10:44 +0100

    Handle SIGINT for clean termination.

--- main.go
+++ main.go
@@ -6,27 +6,51 @@ import (
 	// "os"
 	// "runtime/trace"
 	"log"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 )
 
 func main() {
 	executionTimeLimit := time.Second * 30
-	flushTimeLimit := time.Second * 15
+	gracePeriod := time.Second * 15
 
 	// trace.Start(os.Stdout)
 	wg := sync.WaitGroup{}
 	ctx, cancel := context.WithCancel(context.Background())
+	signalsCh := setupTermination(cancel)
 
 	pipeline.Run(ctx, time.Second*5, &wg)
 
-	time.Sleep(executionTimeLimit)
 	// We won't ever wait for this one.
 	go func() {
-		log.Println("Terminating...")
-		cancel()
+		time.Sleep(executionTimeLimit)
+		signalsCh <- syscall.SIGINT // Simulate Ctrl+C for our tracing.
 	}()
 
+	waitToTerminate(&wg, gracePeriod)
+
+	// trace.Stop()
+}
+
+func setupTermination(cancel context.CancelFunc) chan os.Signal {
+	signalsCh := make(chan os.Signal, 64)
+	signal.Notify(signalsCh, syscall.SIGINT)
+	// We won't ever wait for this one.
+	go func() {
+		select {
+		case <-signalsCh:
+			log.Println("Terminating...")
+			cancel()
+		}
+
+	}()
+	return signalsCh
+}
+
+func waitToTerminate(wg *sync.WaitGroup, gracePeriod time.Duration) {
 	// Just to stop when tracing.
 	barrierCh := make(chan struct{})
 	go func() {
@@ -36,9 +60,7 @@ func main() {
 	select {
 	case <-barrierCh:
 		log.Println("Pipeline completed")
-	case <-time.After(flushTimeLimit):
+	case <-time.After(gracePeriod):
 		log.Println("Timeout waiting for finish")
 	}
-
-	// trace.Stop()
 }

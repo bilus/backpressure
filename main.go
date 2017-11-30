caf431fac18f516290483f4d6eec5b0b03a82ff1
Marcin Bilski <gyamtso@gmail.com>
2017-11-30 09:46:58 +0100

    Add (commented-out) tracing.

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"github.com/nowthisnews/dp-pubsub-archai/pipeline"
+	// "os"
+	// "runtime/trace"
 	"time"
 )
 
 func main() {
+	// trace.Start(os.Stdout)
 	pipeline.Run(time.Second * 5)
+	// trace.Stop()
 }

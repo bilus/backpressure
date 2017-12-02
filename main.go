a492e669959ce9afecac87b4bc3f466efe1b05e7
Marcin Bilski <gyamtso@gmail.com>
2017-12-02 14:31:23 +0100

    Config customization example.

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 
 	"github.com/bilus/backpressure/pipeline/runner"
 	"golang.org/x/net/context"
+	"time"
 	// "runtime/trace"
 )
 
 func main() {
 	// trace.Start(os.Stdout)
 	config := runner.DefaultConfig()
-	runner.RunPipeline(context.Background(), config, fake.TaskProducer{500}, fake.BatchConsumer{500})
+	config.DispatchTick = time.Millisecond * 100
+	config.TaskQueueSize = 128
+	runner.RunPipeline(context.Background(), config, fake.TaskProducer{1}, fake.BatchConsumer{10})
 	// trace.Stop()
 }
